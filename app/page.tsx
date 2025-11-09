64e0283eed247f0bb4ad6c71d351b1813e5b48a1
PradhumnaPancholi <pnpancholi@protonmail.com>
2025-11-09 18:48:13 +0530

    added content for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,52 @@ export default function Home() {
     <>
       <Navbar />
       <main className="flex-grow">
-        <p>Home page</p>
+        {/*hero section*/}
+        <div className="hero min-h-[70vh] bg-base-200">
+          <div className="hero-content grid lg:grid-cols-2 gap-8 items-center justify-items-center">
+            {/*Left Content*/}
+            <div className="max-w-md ">
+              <h1 className="text-5xl font-bold">
+                You Next Startup Idea Starts Here
+              </h1>
+              <p className="py-6">
+                Launch your groundbreaking venture! Build your app from concept
+                to market
+              </p>
+              <button className="btn btn-primary btn-lg">Get Started</button>
+            </div>
+            {/*Right Content*/}
+            <div className="hidden lg:block">
+              <div className="mockup-code bg-neutral text-neutral-content rounded-box shadow-2xl overflow-x-auto">
+                <pre data-prefix="$" className="text-info">
+                  <code>
+                    git clone
+                    https://github.com/PradhumnaPancholi/the-next-startup
+                  </code>
+                </pre>
+                <pre data-prefix="$" className="text-info">
+                  cd the-next-startup
+                </pre>
+                <pre data-prefix="$" className="text-info">
+                  pnpm install
+                </pre>
+                <pre data-prefix="$" className="text-success">
+                  <code>Start Building!!!</code>
+                </pre>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*Featres*/}
+        <section className="py-16 bg-base-100 text-base-content">
+          <div className="container mx-auto px-4 text-center">
+            <h2 className="text-4xl font-bold mb-4">Features</h2>
+            <p className="text-lg mb-12 max-w-2xl mx-auto">
+              Out of the box, you get
+            </p>
+            {/*grid to showcase features*/}
+          </div>
+        </section>
       </main>
       <Footer />
     </>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,24 +1,23 @@
-import {betterAuth} from "better-auth";
-import {drizzleAdapter} from "better-auth/adapters/drizzle";
-import {db} from "./db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
 import * as schema from "@/schema/auth";
-
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema, // important for mapping tables
   }),
-  emailAndPassword: {enabled: true},
+  emailAndPassword: { enabled: true },
   // logging
   callbacks: {
     onError: (error: any) => {
       console.error("Better Auth Error:", error);
     },
-    onSignUp: ({ user }:any) => {
+    onSignUp: ({ user }: any) => {
       console.log("Sign-up success:", user.email);
     },
-    onSignIn: ({ user}:any) => {
+    onSignIn: ({ user }: any) => {
       console.log("Sign-in success:", user.email);
     },
   },
@@ -27,5 +26,5 @@ export const auth = betterAuth({
     autoSetup: true, // creates user , sessions, etc
   },
   //ToDo: Add email verification//
-  magicLink: {enabled: true}
+  magicLink: { enabled: true }
 });
