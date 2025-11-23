3850d27e2947d4326050149a6fb4f12d9030f21e
PradhumnaPancholi <pnpancholi@protonmail.com>
2025-11-23 15:20:55 +0530

    added features section, will be updated with more copy-writing approach

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,54 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-
+import {
+  LockClosedIcon,
+  ShieldCheckIcon,
+  CodeBracketIcon,
+  SwatchIcon,
+  SparklesIcon,
+  RocketLaunchIcon,
+} from "@heroicons/react/24/solid";
 export default function Home() {
+  const featuresList = [
+    {
+      icon: ShieldCheckIcon,
+      title: "Auth that just works",
+      description:
+        "Sign-up, Sign-in, Magic Links, Password Reset and Social Logins - all ready to go. And here's more, no vendor lock-in and you own the data",
+    },
+    {
+      icon: LockClosedIcon,
+      title: "Zero Vendor Prison",
+      description:
+        "Supabase today, Neon tomorrow, Railway next week — Switching to the data platform that meet your needs is super easy",
+    },
+    {
+      icon: RocketLaunchIcon,
+      title: "Prototype in Hours",
+      description:
+        "Stop copy-pasting boilerplate. Apex Kit handles the boring and repetive stuff for yur. So, you can focus on your core feature and product-market fit.",
+    },
+    {
+      icon: CodeBracketIcon,
+      title: "Type-Safe End-to-End ",
+      description:
+        "Drizzle + TypeScript = no more “any” surprises. Catch bugs before they catch your users (or your sleep).",
+    },
+    {
+      icon: SwatchIcon,
+      title: "Themes Out of the Box",
+      description:
+        "Light, dark, cyberpunk, cupcake — your MVP doesn't need to look bland or unappealing. Extend to your own design language when your team is ready",
+    },
+    {
+      icon: SparklesIcon,
+      title: "Built for 2026",
+      description:
+        "A healthy mix of modern and battle-tested approaches. Build to provide mordern developer experience without sacrificing security",
+    },
+  ];
   return (
     <>
       <main className="flex-grow">
-        {/*hero section*/}
+        {/*Hero section*/}
         <div className="hero min-h-[70vh] bg-base-200">
           <div className="hero-content grid lg:grid-cols-2 gap-8 items-center justify-items-center">
             {/*Left Content*/}
@@ -46,9 +89,25 @@ export default function Home() {
           <div className="container mx-auto px-4 text-center">
             <h2 className="text-4xl font-bold mb-4">Features</h2>
             <p className="text-lg mb-12 max-w-2xl mx-auto">
-              Out of the box, you get
+              Everything you need, Nothing you don't
             </p>
             {/*grid to showcase features*/}
+            <div className="grid md:grid-cols-3 gap-8 max-w-5xl mx-auto">
+              {featuresList.map((feature, index) => (
+                <div
+                  key={index}
+                  className="card bg-base-200 shadow-xl hover:shadow-2xl transition-all duration-300 hover:-translate-y-1"
+                >
+                  <div className="card-body items-center text-center">
+                    <feature.icon className="text-primary h-8 w-8" />
+                    <h3 className="card-title text-xl mb-3">{feature.title}</h3>
+                    <p className="text-base-content/80">
+                      {feature.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       </main>
