d4deb102c09f2fdf4379a7ebbe02f71a8c6d8c80
PradhumnaPancholi <pnpancholi@protonmail.com>
2025-11-23 16:52:25 +0530

    temp fix for build fail

--- app/reset-password/actions.ts
+++ app/reset-password/actions.ts
@@ -2,12 +2,12 @@ import { auth } from "@/lib/auth";
 
 export async function resetPassword(formdata: FormData) {
   const password = formdata.get("password") as string;
-  try {
-    const data = await auth.api.resetPassword({
-      body: { newPassword: password, token: token },
-    });
-  } catch (error) {
-    console.error("Server Actions (Reset Password): ", error);
-    return;
-  }
+  // try {
+  //   const data = await auth.api.resetPassword({
+  //     body: { newPassword: password, token: token },
+  //   });
+  // } catch (error) {
+  //   console.error("Server Actions (Reset Password): ", error);
+  //   return;
+  // }
 }
