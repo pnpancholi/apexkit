a3b6f98513fd2001c98538cfa8f55e70798c5f6c
PradhumnaPancholi <pnpancholi@protonmail.com>
2025-11-23 16:24:11 +0530

    added made by ribbon

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     },
     {
       icon: LockClosedIcon,
-      title: "Zero Vendor Prison",
+      title: "Avoid Vendor Prison",
       description:
         "Supabase today, Neon tomorrow, Railway next week — Switching to the data platform that meet your needs is super easy",
     },
@@ -45,11 +45,34 @@ export default function Home() {
         "A healthy mix of modern and battle-tested approaches. Build to provide mordern developer experience without sacrificing security",
     },
   ];
+  const builtWithTools = [
+    {
+      name: "Next JS",
+      imageURL:
+        "https://assets.vercel.com/image/upload/v1662130272/nextjs-black-logo.svg",
+    },
+    {
+      name: "TypeScript",
+      imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+    },
+    {
+      name: "Tailwind CSS",
+      imageURL: "https://tailwindcss.com/favicons/favicon-32x32.png",
+    },
+
+    { name: "daisy UI", imageURL: "https://daisyui.com/favicon.ico" },
+    { name: "Better Auth", imageURL: "https://www.better-auth.com/logo.svg" },
+    {
+      name: "Drizzle",
+      imageURL: "https://drizzle.team/assets/logo.svg",
+    },
+  ];
   return (
     <>
       <main className="flex-grow">
-        {/*Hero section*/}
-        <div className="hero min-h-[70vh] bg-base-200">
+        {/*Hero Section*/}
+        <section className="hero min-h-[70vh] bg-base-200">
           <div className="hero-content grid lg:grid-cols-2 gap-8 items-center justify-items-center">
             {/*Left Content*/}
             <div className="max-w-md ">
@@ -83,8 +106,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
-        {/*Featres*/}
+        </section>
+        {/*Featres Section*/}
         <section className="py-16 bg-base-100 text-base-content">
           <div className="container mx-auto px-4 text-center">
             <h2 className="text-4xl font-bold mb-4">Features</h2>
@@ -110,6 +133,44 @@ export default function Home() {
             </div>
           </div>
         </section>
+        {/*Built With Section*/}
+        <section className="py-20 bg-base-200">
+          <div className="container mx-auto px-6 text-center">
+            <h3 className="text-3xl font-bold mb-4">
+              Built on the mordern ecosystem, loved by developers
+            </h3>
+            <p className="text-xl text-base-content/70 mb-12 max-w-2xl mx-auto">
+              Lorem ipsum.
+            </p>
+
+            <div className="flex flex-wrap justify-center items-center gap-10 md:gap-16 opacity-80">
+              {builtWithTools.map((tool, index) => (
+                <a
+                  key={index}
+                  className="flex flex-col justify-center items-center gap-10 md:gap-16"
+                >
+                  <img
+                    src={tool.imageURL}
+                    alt={tool.name}
+                    className="h-20 mb-2 object-contain"
+                  />
+                  <p className="text-sm">{tool.name}</p>
+                </a>
+              ))}
+            </div>
+          </div>
+        </section>
+        {/*Made by - ribbon footer*/}
+        <p className="text-sm text-base-content/60 text-center py-12">
+          Built with{" "}
+          <span className="font-bold text-primary">sheer stubbornness</span>— by{" "}
+          <a
+            href="https://twitter.com/knowpradhumna"
+            className="font-semibold text-primary hover:underline"
+          >
+            Pradhumna Pancholi (Prad)
+          </a>
+        </p>
       </main>
     </>
   );
