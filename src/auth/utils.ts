34cfcc2f83e69f9ce7db4768fd864b540b88f1b8
PradhumnaPancholi <pnpancholi@protonmail.com>
2026-01-13 15:45:19 +0530

    refactored dir strcture

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -1,24 +1,6 @@
 import { auth } from "@/lib/auth";
-import { Session } from "better-auth/types";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
-/**
- * Get the current authenticated session
- * This is the core function that talks to Better Auth
- */
-//export async function getSession() {
-//  try {
-//    const session = await auth.api.getSession({
-//      headers: await headers(),
-//    });
-//    console.log("Session", session);
-//    return session as AppSession | null;
-//  } catch (error) {
-//    console.error("Auth-Utils: failed to get session", error);
-//    return null;
-//  }
-//}
 
 export async function getUser() {
   try {
