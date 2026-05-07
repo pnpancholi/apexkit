47b59e6f4de134c4751f9d9f992f23008a7b101d
PradhumnaPancholi <pnpancholi@protonmail.com>
2026-05-07 12:02:27 +0530

    made a custom Button component for easy refacotring ,cleaner page, and ease of maintenance

---
+++ src/Button.tsx
@@ -0,0 +1,61 @@
+import { ReactNode } from 'react'
+
+interface ButtonProps {
+  children: ReactNode
+  color: 'primary' | 'secondary' | 'accent' | 'success' | 'error'
+  variant: 'regular' | 'outlined'
+  size: 'sm' | 'md' | 'lg'
+  isLoading?: boolean
+  disabled?: boolean
+  onClick?: () => void
+  type: 'button' | 'submit' | 'reset'
+  className?: string
+}
+export default function Button({
+  children,
+  color = 'primary',
+  variant = 'regular',
+  size = 'md',
+  type = 'button',
+  isLoading = false,
+  disabled = false,
+  onClick,
+  className = '',
+}: ButtonProps) {
+  const baseClasses = 'btn font-semibold transiton-all'
+
+  const colorClasses = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+    accent: 'btn-accent',
+    success: 'btn-success',
+    error: 'btn-error',
+  }
+  const sizeClasses = {
+    sm: 'btn-sm',
+    md: 'btn-md',
+    lg: 'btn-lg',
+  }
+
+  const variantClasses = {
+    regular: 'btn',
+    outlined: 'btn-outline',
+  }
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled || isLoading}
+      className={`${baseClasses} ${sizeClasses[size]} ${variantClasses[variant]} ${colorClasses[color]} ${className}}`}
+    >
+      {isLoading ? (
+        <>
+          <span className="loading loading-spinner"></span>
+          Loading...
+        </>
+      ) : (
+        children
+      )}
+    </button>
+  )
+}
