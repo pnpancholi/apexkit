ec946fba9a764331f3cfb98d4f29cbd74d265df9
PradhumnaPancholi <pnpancholi@protonmail.com>
2026-05-10 22:23:08 +0530

    refactored update email to use Modal component

--- src/app/profile/UpdateEmail.tsx
+++ src/app/profile/UpdateEmail.tsx
@@ -1,3 +1,39 @@
+'use client'
+
+import React from 'react'
+import Button from '@/components/ui/Button'
+import Modal from '@/components/layout/Modal'
+import Input from '@/components/ui/Input'
+import Alert from '@/components/ui/Alert'
+
 export default function UpdateEmail() {
-  return <div></div>
+  const [isOpen, setOpen] = React.useState(false)
+
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(true)}
+        color="accent"
+        variant="outlined"
+        type="button"
+        className="w-full mt-4"
+      >
+        Update Email
+      </Button>
+      <Modal isOpen={isOpen} onClose={() => setOpen(false)} title="Update Email ">
+        <div className="join w-full my-10">
+          <Input
+            type="email"
+            placeholder="Enter your new email"
+            className="grow join-item"
+            name="email"
+            required
+          />
+          <Button type="button" color="primary" className="join-item">
+            Update
+          </Button>
+        </div>
+      </Modal>
+    </>
+  )
 }
--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -7,16 +7,31 @@ interface ModalProps {
   onClose: () => void
 }
 
-export default function Modal({ title = '', children }: ModalProps) {
-  const backdropClasses = 'rounded-none shadow-xl backdrop:bg-black/50'
-  const containerClasses = 'max-w-md w-full mx-4'
-  const headerClasses = 'px-6 py-4 border-b border-base-200'
-  const contentClasses = 'px-6 py-4'
+// why use effect, simple dep, no race condition, no async, why dialog//
+export default function Modal({ title = '', isOpen = false, onClose, children }: ModalProps) {
+  const baseClasses = 'modal outline:none backdrop:bg-black/50 focus:border-none'
+  const containerClasses = 'modal-box rounded-none shadow-xl max-w-md w-full mx-4'
+  const headerClasses = 'capitalize py-2 border-b border-text-200 text-center text-lg font-semibold'
+  const contentClasses = ''
+
+  const dialogRef = React.useRef<HTMLDialogElement>(null)
+
+  const handleBackdropClick = (e: React.MouseEvent<HTMLDialogElement>) => {
+    if (e.target === e.currentTarget) onClose()
+  }
+
+  React.useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal()
+    } else {
+      dialogRef.current?.close()
+    }
+  }, [isOpen])
 
   return (
-    <dialog className={backdropClasses}>
+    <dialog onClose={onClose} ref={dialogRef} onClick={handleBackdropClick} className={baseClasses}>
       <div className={containerClasses}>
-        {title && <div className={headerClasses}>{title}</div>}
+        {title && <h2 className={headerClasses}>{title}</h2>}
         <div className={contentClasses}>{children}</div>
       </div>
     </dialog>
