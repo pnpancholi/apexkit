9e9f641a3972d9ba9ad98ef9da59656be8acc20d
PradhumnaPancholi <pnpancholi@protonmail.com>
2026-05-14 22:50:26 +0530

    fixed tests to match new reset password function pattern

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,88 +1,62 @@
 import Link from 'next/link'
-import type React from 'react'
 import type { User } from '@/types/auth'
+import { FaBarsStaggered } from 'react-icons/fa6'
 
-const Navbar: React.FC<{ user: User | null }> = async ({ user }) => {
-  const navLinks = [
-    {
-      name: 'blog',
-      url: '/blog',
-    },
-    {
-      name: 'about',
-      url: '/about',
-    },
-    {
-      name: 'contact',
-      url: '/contact',
-    },
-  ]
+const navLinks = [
+  {
+    name: 'blog',
+    url: '/blog',
+  },
+  {
+    name: 'about',
+    url: '/about',
+  },
+  {
+    name: 'contact',
+    url: '/contact',
+  },
+]
+
+export default function Navbar({ user }: { user: User | null }) {
+  const navLinkClasses =
+    'capitalize relative after:absolute after:left-1/2 after:bottom-0 after:h-0.5 after:w-0 after:-translate-x-1/2 after:bg-primary after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 hover:after:translate-x-0'
 
   return (
-    <div className="navbar fixed top-0 w-full shadow-md px-6 lg:px-40 bg-base-200 z-[20] border-b-text">
+    <nav className="navbar fixed top-0 w-full shadow-md px-6 lg:px-40 bg-base-200 z-20 border-b-text">
       <div className="navbar-start">
-        <a className="btn btn-ghost normal-case text-xl" href="/">
+        <Link className="btn btn-ghost normal-case text-xl" href="/">
           ApexKit
-        </a>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal">
           {!user &&
             navLinks.map((link) => (
               <li key={link.name}>
-                <a
-                  href={link.url}
-                  className=" capitalize relative after:absolute after:left-1/2 after:bottom-0 after:h-0.5 after:w-0 after:-translate-x-1/2 after:bg-primary after:transition-all after:duration-300 hover:after:w-full hover:after:left-0 hover:after:translate-x-0"
-                >
+                <Link href={link.url} className={navLinkClasses}>
                   {link.name}
-                </a>
+                </Link>
               </li>
             ))}
         </ul>
       </div>
       <div className="navbar-end">
         <div className="dropdown">
-          <label htmlFor="navbar-menu" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <title>Menu</title>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul className="menu menu-compact dropdown-content w-screen fixed z-50 mt-3 p-4 shadow bg-base-100 inset-x-0 rounded-none items-center">
-            <li>
-              <a href="/blog" className="text-lg font-medium">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="text-lg font-medium">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="text-lg font-medium">
-                Contact
-              </a>
-            </li>
+          <button type="button" tabIndex={0} className="btn btn-ghost lg:hidden">
+            <FaBarsStaggered />
+          </button>
+          <ul className="menu menu-compact bg-base-200 dropdown-content w-screen fixed z-50 space-y-6 p-4 shadow inset-x-0 rounded-none items-center">
+            {navLinks.map((navItem) => (
+              <li key={navItem.name} className={navLinkClasses}>
+                <Link href={navItem.url}>{navItem.name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
         {user ? (
           <Link href="/profile">
-            <div className="avatar placeholder">
-              <div className="flex items-center justify-center bg-primary text-primary-content rounded-full w-10">
-                <span className="text-sm">{user.name?.charAt(0).toUpperCase()}</span>
-              </div>
+            <div className="bg-primary text-primary-content rounded-full w-10 h-10 flex items-center justify-center">
+              <span className="text-sm">{user.name?.charAt(0).toUpperCase()}</span>
             </div>
           </Link>
         ) : (
@@ -91,8 +65,6 @@ const Navbar: React.FC<{ user: User | null }> = async ({ user }) => {
           </Link>
         )}
       </div>
-    </div>
+    </nav>
   )
 }
-
-export default Navbar
